3cb3b8e39dbce42501ee6312706c865354a169e9
Nycolas de Medeiros <nycolas.medeiros@gmail.com>
2025-02-02 12:22:03 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let novoAmigo = document.getElementById("amigo").value;
+
+  if(novoAmigo === ""){
+    alert("Por favor, digite um nome;");
+  } else {
+    amigos.push(novoAmigo);
+    document.getElementById("amigo").value = "";
+  }
+  atualizarListaDeNomes();
+}
+
+function atualizarListaDeNomes() {
+  let listaDeNomes = document.getElementById("listaAmigos");
+  listaDeNomes.innerHTML = "";
+  
+  for(let i = 0; i < amigos.length; i++){
+    let novoLi = document.createElement("li");
+    novoLi.innerHTML = amigos[i];
+    listaDeNomes.append(novoLi);
+  }
+}
+
+
